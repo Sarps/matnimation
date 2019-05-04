26e70960b1adfbbbec41334eb82c0388f92e6f23
Emmanuel Oppong-Sarpong <esarpong51@gmail.com>
2019-05-04 22:34:24 +0000

    Added font and icons as local dependency

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import store from './store'
 import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
